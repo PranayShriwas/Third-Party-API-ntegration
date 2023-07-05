a96c157f861daabbd445a2b066a950c2e6c9907c
PranayShriwas <pranayshriwas420@gmail.com>
2023-07-05 20:38:53 +0530

    ThirdPartyApi

---
+++ thirdParty/app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+import requests
+import json
+# Create your views here.
+
+
+def index(request):
+    url = 'https://princestudentapi.onrender.com/AddStudent//'
+    response = requests.get(url)
+    data = json.loads(response.text)
+    return render(request, 'index.html', {"data": data})
+
+# def index(request):
+#     return render(request, 'index.html')
